6b57a1da4f7e0d12314997327b9b10c469d5f43f
viratkumar <virat.kumar@olx.in>
2025-07-19 14:33:11 +0530

    Changes

--- src/nativeMain/kotlin/org/corexero/metroui/MetroUiViewController.kt
+++ src/nativeMain/kotlin/org/corexero/metroui/MetroUiViewController.kt
@@ -1,9 +1,9 @@
 package org.corexero.metroui
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
-import androidx.compose.ui.interop.LocalUIViewController
-import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.uikit.LocalUIViewController
 import org.corexero.metroui.data.datastore.DataStoreManager
 import org.corexero.metroui.data.datastore.MetroDataStoreFactory
 import org.corexero.metroui.data.repositoryImpl.FirebaseAnalyticsTracker
@@ -18,36 +18,35 @@ import org.corexero.metroui.domain.utils.IntentUtils
 import org.corexero.metroui.presentation.App
 import org.corexero.metroui.ui.MetroData
 import org.corexero.metroui.ui.MetroDataProvider
-import platform.UIKit.UIViewController
 
+@Composable
 fun MetroUiViewController(
     stationRepository: StationRepository,
     routeRepository: RouteRepository,
     metroBrandConfig: MetroBrandConfig
-): UIViewController =
-    ComposeUIViewController {
-        val localUIViewController = LocalUIViewController.current
-        val intentUtils = remember { IntentUtils(localUIViewController) }
-        val dataStoreManager = remember { DataStoreManager(MetroDataStoreFactory()) }
-        val recentSearchRepository = remember { RecentSearchRepositoryImpl(dataStoreManager) }
-        val locationRepository = remember { LocationRepositoryImpl() }
-        val preferenceRepository = remember { PreferenceRepositoryImpl(dataStoreManager) }
-        val metroData = remember {
-            MetroData(
-                intentUtils = intentUtils,
-                recentSearchRepository = recentSearchRepository,
-                locationRepository = locationRepository,
-                featureFlagRepository = FirebaseFeatureFlag,
-                analyticsTracker = FirebaseAnalyticsTracker,
-                preferenceRepository = preferenceRepository,
-                stationRepository = stationRepository,
-                routeRepository = routeRepository,
-                metroBrandConfig = metroBrandConfig
-            )
-        }
-        CompositionLocalProvider(
-            MetroDataProvider provides metroData
-        ) {
-            App()
-        }
+) {
+    val localUIViewController = LocalUIViewController.current
+    val intentUtils = remember { IntentUtils(localUIViewController) }
+    val dataStoreManager = remember { DataStoreManager(MetroDataStoreFactory()) }
+    val recentSearchRepository = remember { RecentSearchRepositoryImpl(dataStoreManager) }
+    val locationRepository = remember { LocationRepositoryImpl() }
+    val preferenceRepository = remember { PreferenceRepositoryImpl(dataStoreManager) }
+    val metroData = remember {
+        MetroData(
+            intentUtils = intentUtils,
+            recentSearchRepository = recentSearchRepository,
+            locationRepository = locationRepository,
+            featureFlagRepository = FirebaseFeatureFlag,
+            analyticsTracker = FirebaseAnalyticsTracker,
+            preferenceRepository = preferenceRepository,
+            stationRepository = stationRepository,
+            routeRepository = routeRepository,
+            metroBrandConfig = metroBrandConfig
+        )
     }
+    CompositionLocalProvider(
+        MetroDataProvider provides metroData
+    ) {
+        App()
+    }
+}
