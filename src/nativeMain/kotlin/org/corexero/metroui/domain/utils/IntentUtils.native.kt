1fdc674c83a47cc2513c759d5413f601dd1f9fef
viratkumar <viratdevx@gmail..com>
2025-08-22 13:58:04 +0530

    Corrected uri open crash

--- src/nativeMain/kotlin/org/corexero/metroui/domain/utils/IntentUtils.native.kt
+++ src/nativeMain/kotlin/org/corexero/metroui/domain/utils/IntentUtils.native.kt
@@ -3,30 +3,54 @@ package org.corexero.metroui.domain.utils
 import androidx.compose.ui.graphics.ImageBitmap
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.refTo
+import kotlinx.cinterop.useContents
 import platform.CoreGraphics.CGBitmapContextCreate
 import platform.CoreGraphics.CGBitmapContextCreateImage
 import platform.CoreGraphics.CGColorSpaceCreateDeviceRGB
 import platform.CoreGraphics.CGImageAlphaInfo
+import platform.CoreGraphics.CGRectMake
+import platform.Foundation.NSThread
 import platform.UIKit.UIActivityViewController
 import platform.UIKit.UIImage
+import platform.UIKit.UIPopoverArrowDirection
+import platform.UIKit.UIPopoverArrowDirectionAny
 import platform.UIKit.UIViewController
 import platform.UIKit.popoverPresentationController
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
 
 actual class IntentUtils(
     private val uiViewController: UIViewController
 ) {
 
+    @OptIn(ExperimentalForeignApi::class)
     actual fun onShareMetroRoute(imageBitmap: ImageBitmap) {
+
+        val presenter = uiViewController.presentedViewController ?: uiViewController
+
         convertImageBitmapToUIImage(imageBitmap)?.let { uiImage ->
             val activityVC = UIActivityViewController(
                 activityItems = listOf(uiImage),
                 applicationActivities = null
             )
-            activityVC.popoverPresentationController?.sourceView = uiViewController.view
-            uiViewController.presentViewController(
-                activityVC,
-                true,
-                null
+            activityVC.popoverPresentationController?.let { pop ->
+                pop.sourceView = presenter.view
+                val bounds = presenter.view.bounds
+                val w: Double = bounds.useContents { size.width }
+                val h: Double = bounds.useContents { size.height }
+                pop.sourceRect = CGRectMake(w / 2.0, h / 2.0, 1.0, 1.0)
+                pop.permittedArrowDirections = UIPopoverArrowDirectionAny
+            }
+            val presentBlock = {
+                uiViewController.presentViewController(
+                    presenter,
+                    true,
+                    null
+                )
+            }
+            if (NSThread.isMainThread()) presentBlock() else dispatch_async(
+                dispatch_get_main_queue(),
+                presentBlock
             )
         }
 
